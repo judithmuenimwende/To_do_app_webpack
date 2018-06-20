72a1c0b208ff544c9fa0ad4f10a40c624eb2ac4e
Judith Mueni <judithmueni395@gmail.com>
2018-06-20 15:32:45 +0300

    Task manager

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container">
     <h3>{{ title }}</h3>
+    <form>
+      <textarea placeholder="Add new task" v-model="newTask"></textarea>
+      <p>{{newTask}}</p>
+      <button type="button" v-on:click="addTask">Add Task {{clicked}}</button>
+    </form>
+    <ul>
+   <li v-for="task in tasks">{{task}}</li>
+    </ul>
+    
   </div>
 </template>
 
@@ -8,8 +17,17 @@
 export default {
   data: function() {
     return {
-      title: 'My App'
+      title: 'To Do List',
+      newTask:'This is a Judith',
+      clicked: 0,
+      tasks:[]
     }
+  },
+  methods: {
+  	addTask(){
+  	this.tasks.unshift(this.newTask);
+  	this.newTask = '';
+  	}
   }
 }
 </script>
